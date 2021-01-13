99ae0238ac5b3057186968cf03efc2866ec0d9a9
Raphew <raphheuhh@outlook.com>
2021-01-13 18:58:51 +0100

    Hello world !

---
+++ src/main/java/fr/raphew/chatonline/main/Main.java
@@ -0,0 +1,11 @@
+package fr.raphew.chatonline.main;
+
+/**
+ * This file of 'Chat online' was created by Raphew on 13/01/2021
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world !");
+    }
+}
